2d04abc73beceadc3099c415c2588fe1a51ed114
MathIsFun0 <89877664+MathIsFun0@users.noreply.github.com>
2025-02-18 00:01:11 -0500

    all bananas

--- potassium.lua
+++ potassium.lua
@@ -1,6 +1,8 @@
 G.C.BANAN1 = HEX('f5d953')
 G.C.BANAN2 = HEX('9be344')
 
+SMODS.config.no_mod_badges = true
+
 SMODS.Atlas{
     key = "centers",
     path = "Enhancers.png",
@@ -8,3 +10,24 @@ SMODS.Atlas{
     py = 95,
     raw_key = true
 }
+
+-- Add updates to existing Jokers
+SMODS.Joker:take_ownership('j_oops', {
+    name = "Oops! All Bananas",
+    loc_txt = {
+        name = "Oops! All Bananas",
+        text = {
+            "Guarantees all {C:attention}listed",
+            "{C:green,E:1,S:1.1}probabilities",
+            "{C:inactive}(ex: {C:green}1 in 3{C:inactive} -> {C:green}inf in 3{C:inactive})",
+        }
+    },
+    add_to_deck = function(self, card, from_debuff)
+        G.GAME.probabilities.normal = 1e400
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+        if not next(SMODS.find_card('j_oops')) then
+            G.GAME.probabilities.normal = 1
+        end
+    end
+})
