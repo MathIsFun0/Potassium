24e7650e1e9130f48cc758704514ef465980fa5a
MathIsFun0 <89877664+MathIsFun0@users.noreply.github.com>
2025-03-15 22:25:29 -0400

    glop

--- potassium.lua
+++ potassium.lua
@@ -13,13 +13,7 @@ G.trollRate = 100
 SMODS.config.no_mod_badges = true
 
 
---[[G.localization.misc.dictionary.k_plus_stone = "+1 Banana"
-G.localization.misc.dictionary.k_balanced = "Bananalanced"
-G.localization.misc.dictionary.ph_you_win = "BANANA!"
-G.localization.misc.challenge_names.c_medusa_1 = "Bananadusa"
-G.localization.misc.challenge_names.c_monolith_1 = "Bananolith"]]
-
--- Texture replacement
+-- ===Texture replacement===
 SMODS.Atlas{
     key = "centers",
     path = "Enhancers.png",
@@ -35,7 +29,7 @@ SMODS.Atlas{
     raw_key = true
 }
 
--- Add updates to existing Jokers
+-- ===Add updates to existing Jokers===
 SMODS.Joker:take_ownership('j_oops', {
     name = "Oops! All Bananas",
     --[[loc_txt = {
@@ -173,7 +167,7 @@ SMODS.Stake:take_ownership('stake_blue', {
     }
 })
 
--- New Content
+-- ===New Content===
 SMODS.Back{
     key = "banana",
     --[[loc_txt = {
@@ -217,7 +211,7 @@ function get_blind_main_colour(blind) --either in the form of the blind key for 
     blind == 'bl_big' and mix_colours(G.C.ORANGE, G.C.BLACK, 0.6)) or G.C.BLACK
   end
 
---Trolling
+-- ===Trolling===
 function trolled()
     if not G.trollRate then return false end
     math.randomseed(os.time())
@@ -278,7 +272,7 @@ function Game:init_game_object()
     return ret
 end
 
--- Final boss shenanigans
+-- ===Final boss shenanigans===
 local rb = reset_blinds
 function reset_blinds()
     rb()
@@ -356,3 +350,69 @@ SMODS.Blind{
     in_pool = false,
     dollars = 15,
 }
+
+-- ==Glop==
+
+-- Add Glop to UI
+local cuih = create_UIBox_HUD
+function create_UIBox_HUD()
+    local ret = cuih()
+    local hand_UI = ret.nodes[1].nodes[1].nodes[4].nodes[1].nodes[2].nodes
+    -- Cleanly remove existing DynaText (prevent memory leaks)
+    hand_UI[1].nodes[1].config.object:remove()
+    hand_UI[1].nodes[2].config.object:remove()
+    hand_UI[3].nodes[1].config.object:remove()
+    hand_UI[3].nodes[3].config.object:remove()
+    hand_UI = {
+        {n=G.UIT.C, config={align = "cm", minw = 1.3, minh =0.7, r = 0.1,colour = G.C.UI_CHIPS, id = 'hand_chip_area', emboss = 0.05}, nodes={
+            {n=G.UIT.O, config={func = 'flame_handler',no_role = true, id = 'flame_chips', object = Moveable(0,0,0,0), w = 0, h = 0}},
+            {n=G.UIT.O, config={id = 'hand_chips', func = 'hand_chip_UI_set',object = DynaText({string = {{ref_table = G.GAME.current_round.current_hand, ref_value = "chip_text"}}, colours = {G.C.UI.TEXT_LIGHT}, font = G.LANGUAGES['en-us'].font, shadow = true, float = true, scale = 0.3*2.3})}},
+        }},
+        {n=G.UIT.C, config={align = "cm"}, nodes={
+          {n=G.UIT.T, config={text = "X", lang = G.LANGUAGES['en-us'], scale = 0.6, colour = G.C.UI_MULT, shadow = true}},
+        }},
+        {n=G.UIT.C, config={align = "cm", minw = 1.3, minh=0.7, r = 0.1,colour = G.C.UI_MULT, id = 'hand_mult_area', emboss = 0.05}, nodes={
+          {n=G.UIT.O, config={func = 'flame_handler',no_role = true, id = 'flame_mult', object = Moveable(0,0,0,0), w = 0, h = 0}},
+          {n=G.UIT.O, config={id = 'hand_mult', func = 'hand_mult_UI_set',object = DynaText({string = {{ref_table = G.GAME.current_round.current_hand, ref_value = "mult_text"}}, colours = {G.C.UI.TEXT_LIGHT}, font = G.LANGUAGES['en-us'].font, shadow = true, float = true, scale = 0.3*2.3})}},
+        }},
+        {n=G.UIT.C, config={align = "cm"}, nodes={
+          {n=G.UIT.T, config={text = "X", lang = G.LANGUAGES['en-us'], scale = 0.6, colour = G.C.UI_MULT, shadow = true}},
+        }},
+        {n=G.UIT.C, config={align = "cm", minw = 1.3, minh=0.7, r = 0.1,colour = G.C.UI_GLOP, id = 'hand_glop_area', emboss = 0.05}, nodes={
+          {n=G.UIT.O, config={func = 'flame_handler',no_role = true, id = 'flame_glop', object = Moveable(0,0,0,0), w = 0, h = 0}},
+          {n=G.UIT.O, config={id = 'hand_glop', func = 'hand_glop_UI_set',object = DynaText({string = {{ref_table = G.GAME.current_round.current_hand, ref_value = "glop_text"}}, colours = {G.C.UI.TEXT_LIGHT}, font = G.LANGUAGES['en-us'].font, shadow = true, float = true, scale = 0.3*2.3})}},
+        }},
+    }
+    ret.nodes[1].nodes[1].nodes[4].nodes[1].nodes[2].nodes = hand_UI
+    return ret
+end
+
+G.C.GLOP = HEX('11ff11')
+G.C.UI_GLOP = G.C.GLOP
+G.FUNCS.hand_glop_UI_set = function(e)
+    local new_glop_text = number_format(G.GAME.current_round.current_hand.glop)
+    if new_glop_text ~= G.GAME.current_round.current_hand.glop_text then 
+      G.GAME.current_round.current_hand.glop_text = new_glop_text
+      e.config.object.scale = scale_number(G.GAME.current_round.current_hand.glop, 0.69, 1000)
+      e.config.object:update_text()
+      if not G.TAROT_INTERRUPT_PULSE then G.FUNCS.text_super_juice(e, math.max(0,math.floor(math.log10(type(G.GAME.current_round.current_hand.glop) == 'number' and G.GAME.current_round.current_hand.glop or 1)))) end
+    end
+  end
+
+-- Passive glop functionality - scales with cards triggered
+local ce = SMODS.calculate_effect
+local c_keys = {'chips', 'h_chips', 'chip_mod',
+    'mult', 'h_mult', 'mult_mod',
+    'x_chips', 'xchips', 'Xchip_mod',
+    'x_mult', 'Xmult', 'xmult', 'x_mult_mod', 'Xmult_mod',
+    'e_mult', 'e_chips', 'ee_mult', 'ee_chips', 'eee_mult', 'eee_chips', 'hyper_mult', 'hyper_chips',
+    'emult', 'echips', 'eemult', 'eechips', 'eeemult', 'eeechips', 'hypermult', 'hyperchips',
+    'Emult_mod', 'Echip_mod', 'EEmult_mod', 'EEchip_mod', 'EEEmult_mod', 'EEEchip_mod', 'hypermult_mod', 'hyperchip_mod'}
+function SMODS.calculate_effect(effect, ...)
+    ce(effect, ...)
+    for _, key in ipairs(c_keys) do --scoring effects
+        if effect[key] and glop then
+            glop = glop + 0.01
+        end
+    end
+end
